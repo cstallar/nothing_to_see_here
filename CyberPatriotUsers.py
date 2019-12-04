c5e1d863496a902fe83d3a2dfab21260766ce6f2
cstallar <38629223+cstallar@users.noreply.github.com>
2019-12-03 23:52:52 -0500

    one last one

--- CyberPatriotUsers.py
+++ CyberPatriotUsers.py
@@ -2,8 +2,6 @@ import subprocess
 def add_user(uname):
     subprocess.call(["sudo","useradd","-m",uname])
     p = subprocess.Popen(["sudo","passwd",uname])
-    p.communicate("Kachow69\nKachow69\n")
-    p.terminate()
 def del_user(uname):
     subprocess.call(["sudo","userdel","-r",uname])
 def add_admin(uname):
@@ -12,8 +10,8 @@ def del_admin(uname):
     subprocess.call(["sudo","userdel",uname,"sudo"])
 default_users = ['root', 'daemon', 'bin', 'sys', 'sync', 'games', 'man', 'lp', 'mail', 'news', 'uucp', 'proxy', 'www-data', 'backup', 'list', 'irc', 'gnats', 'nobody', 'libuuid', 'syslog', 'messagebus', 'usbmux', 'dnsmasq', 'avahi-autoipd', 'kernoops', 'rtkit', 'saned', 'whoopsie', 'speech-dispatcher', 'avahi', 'lightdm', 'colord', 'hplip', 'pulse']
 
-requested_users = set(input("users: ").split("\n")+default_users)
-requested_admins = set(input("admins: ").split("\n"))
+requested_users = set(input("users: ").split(",")+default_users)#this is bc of weirdness with shell and newlines, otherwise it would make sense to split by newline
+requested_admins = set(input("admins: ").split(","))
 
 current_user_list = subprocess.check_output(['getent', 'passwd']).decode().split("\n")
 print(current_user_list)
@@ -21,7 +19,7 @@ current_user_list = [x.split(":")[0] for x in current_user_list]
 print(current_user_list)
 current_admin_list = subprocess.check_output(['getent', 'group', 'sudo']).decode().split("\n")
 print(current_admin_list)
-current_admin_list = [x.split(":")[3] for x in current_admin_list]
+current_admin_list = [x.split(":")[3] for x in current_admin_list[:-1]]
 print(current_admin_list)
 
 users_to_add = requested_users.difference(current_user_list)
