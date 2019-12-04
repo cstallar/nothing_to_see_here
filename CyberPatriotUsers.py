bad203e943655d79f03290721b99fe19b56282a5
cstallar <38629223+cstallar@users.noreply.github.com>
2019-12-03 23:47:13 -0500

    Add files via upload

--- CyberPatriotUsers.py
+++ CyberPatriotUsers.py
@@ -16,9 +16,13 @@ requested_users = set(input("users: ").split("\n")+default_users)
 requested_admins = set(input("admins: ").split("\n"))
 
 current_user_list = subprocess.check_output(['getent', 'passwd']).decode().split("\n")
+print(current_user_list)
 current_user_list = [x.split(":")[0] for x in current_user_list]
+print(current_user_list)
 current_admin_list = subprocess.check_output(['getent', 'group', 'sudo']).decode().split("\n")
+print(current_admin_list)
 current_admin_list = [x.split(":")[3] for x in current_admin_list]
+print(current_admin_list)
 
 users_to_add = requested_users.difference(current_user_list)
 print(users_to_add)
