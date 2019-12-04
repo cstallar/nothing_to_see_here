cccbf0326a8487ac62d371b01d799fce37036dae
cstallar <38629223+cstallar@users.noreply.github.com>
2019-12-04 00:18:01 -0500

    Final Commit
    print statements for debug are gone

--- CyberPatriotUsers.py
+++ CyberPatriotUsers.py
@@ -15,38 +15,27 @@ requested_users = set(input("users: ").split(",")+default_users)#this is bc of w
 requested_admins = set(input("admins: ").split(","))
 
 current_user_list = subprocess.check_output(['getent', 'passwd']).decode().split("\n")
-print(current_user_list)
 current_user_list = set([x.split(":")[0] for x in current_user_list])
 if '' in current_user_list:
     current_user_list.remove('')
-print(current_user_list)
+
 current_admin_list = subprocess.check_output(['getent', 'group', 'sudo']).decode().split(",")
-print(current_admin_list)
 current_admin_list = set([x.split(":")[-1].strip() for x in current_admin_list])
 if '' in current_admin_list:
     current_admin_list.remove('')
-print(current_admin_list)
 
 users_to_add = requested_users.difference(current_user_list)
-print(users_to_add)
-input()
 for user in users_to_add:
     add_user(user)
     
 users_to_remove = current_user_list.difference(requested_users)
-print(users_to_remove)
-input()
 for user in users_to_remove:
     del_user(user)
     
 admins_to_add = requested_admins.difference(current_admin_list)
-print(admins_to_add)
-input()
 for admin in admins_to_add:
     add_admin(admin)
 
 admins_to_remove = current_admin_list.difference(requested_admins)
-print(admins_to_remove)
-input()
 for admin in admins_to_remove:
     del_admin(admin)
